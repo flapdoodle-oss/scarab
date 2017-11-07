cd86125aae6e2f536bd3548be22937d02355689f
Michael Mosmann <michael@mosmann.de>
2017-11-07 14:18:02 +0100

    graph stuff

--- src/test/java/se/jbee/sample/TestDependencyGraph.java
+++ src/test/java/se/jbee/sample/TestDependencyGraph.java
@@ -1,26 +1,73 @@
 package se.jbee.sample;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 import org.junit.Test;
 
+import com.google.common.io.Resources;
+
+import de.flapdoodle.graph.GraphAsDot;
+import de.flapdoodle.graph.Graphs;
+import de.flapdoodle.graph.Graphs.GraphBuilder;
 import se.jbee.jvm.Archive;
+import se.jbee.jvm.Method;
 import se.jbee.jvm.Package;
 import se.jbee.jvm.Packages;
 import se.jbee.jvm.file.ClassFile;
 import se.jbee.jvm.file.ClassInputStream;
 import se.jbee.jvm.graph.ClassGraph;
-import se.jbee.jvm.reflect.ClassProcessor;
 
 public class TestDependencyGraph {
 
 	@Test
 	public void dependencyGraph() throws IOException {
-		Archive archive=Archive.NONE;
-		ClassInputStream in=new ClassInputStream(getClass().getResourceAsStream("/scarab/src/test/resources/packages/sample/one-two-three/One.class"));
-		Packages basePackages=Packages.packages(Package.pkg("se"));
-		ClassProcessor out=new ClassGraph(basePackages);
 		
-		ClassFile.readClassfile(archive, in, out);
+		Archive archive=Archive.NONE;
+		
+		Packages basePackages=Packages.packages(Package.pkg("se"));
+		ClassGraph out=new ClassGraph(basePackages);
+		
+		Stream.of("One","Two","Three").forEach(c -> {
+			try {
+				ClassFile.readClassfile(archive, new ClassInputStream(Resources.getResource("packages/sample/one-two-three/"+c+".class").openStream()), out);
+			}
+			catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		
+		out.methods.forEach(node  -> {
+			if (node.calledBy.isEmpty()) {
+				System.out.println("id: "+node.id());
+				node.calls.forEach(calls -> {
+					System.out.println(" -> "+calls.id());
+				});
+			}
+		});
+		
+		GraphBuilder<Method, DefaultEdge, DefaultDirectedGraph<Method, DefaultEdge>> builder = Graphs.graphBuilder(Graphs.directedGraph(Method.class,		DefaultEdge.class)).get();
+		
+		System.out.println("----------------------");
+		
+		out.methods.forEach(node -> {
+			System.out.println(" "+node.id());
+			node.calls.forEach(dest -> {
+				System.out.println(" -> "+dest.id());
+				builder.addVertices(node.id(), dest.id());
+				builder.addEdge(node.id(), dest.id());
+			});
+		});
+		
+		DefaultDirectedGraph<Method, DefaultEdge> graph = builder.build();
+		
+		String graphAsDot = GraphAsDot.builder(Method::toString).build().asDot(graph);
+		
+		System.out.println("----------------------");
+		
+		System.out.println(graphAsDot);
+		
 	}
 }
